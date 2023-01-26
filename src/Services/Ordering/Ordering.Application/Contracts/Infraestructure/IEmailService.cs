25266ffae05a258b4d988fe534b26d529f66758b
João Elias Engelbrecht Kayser <jeliasek@gmail.com>
2023-01-26 18:53:46 -0300

    92. Developing Ordering.Applicaton Layer - Application Contracts

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infraestructure/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Application.Contracts.Models;
+
+namespace Ordering.Application.Contracts.Infraestructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
