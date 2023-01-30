b618ee49416aac745c030c26701b534a23c93efc
João Elias Engelbrecht Kayser <jeliasek@gmail.com>
2023-01-30 18:25:15 -0300

    104 - Register Application and Infraestructure Layer Dependencies into Ordering.API

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,5 @@
 using Ordering.Application;
+using Ordering.Infraestructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplicationServices();
-//builder.Services.AddInfraestructureServices();
+builder.Services.AddInfraestructureServices(builder.Configuration);
 
 var app = builder.Build();
 
