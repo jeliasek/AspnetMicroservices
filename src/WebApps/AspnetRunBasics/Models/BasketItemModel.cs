d6e9cf22314d5e9c8fb011d54b7c776de24fa84c
João Elias Engelbrecht Kayser <jeliasek@gmail.com>
2023-03-22 16:56:41 -0300

    New course. Aula 15 - Adding LoggingDelegatinfHandler for Intercepting Microservices Request

---
+++ src/WebApps/AspnetRunBasics/Models/BasketItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AspnetRunBasics.Models
+{
+    public class BasketItemModel
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
